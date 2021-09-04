3a9446505b651b7be4413d3642468aa544738027
Vincent <vincentwarmerdam@gmail.com>
2021-09-04 07:10:27 +0200

    moar

--- pulldata.py
+++ pulldata.py
@@ -3,8 +3,26 @@ from datasets import load_dataset
 
 for name, subset in [('silicone', 'dyda_da'), ('silicone', 'dyda_e'), ('silicone', 'meld_e')]:
     ds = load_dataset(name, subset)
-    df = pd.concat([
+    df = (pd.concat([
         ds['train'].to_pandas()[['Utterance', 'Label']].assign(split="train"),
-        ds['validation'].to_pandas()[['Utterance', 'Label']].assign(split="valid")
-    ]).rename(columns={'Utterance': 'text', 'Label': 'label'})
+        ds['validation'].to_pandas()[['Utterance', 'Label']].assign(split="valid")])
+          .rename(columns={'Utterance': 'text', 'Label': 'label'}))
     df.to_csv(f"data/{name}-{subset}.csv", index=False)
+
+
+for name, subset in [("tweet_eval", "emoji"), ("tweet_eval", "emotion"), ("tweet_eval", "sentiment")]:
+    ds = load_dataset(name, subset)
+    df = (pd.concat([
+        ds['train'].to_pandas()[['text', 'label']].assign(split="train"),
+        ds['validation'].to_pandas()[['text', 'label']].assign(split="valid")])
+          .assign(text=lambda d: d['text'].str.replace("#", "").str.replace("@", "")))
+    df.to_csv(f"data/{name}-{subset}.csv", index=False)
+
+
+for name, subset in [("liar", None)]:
+    ds = load_dataset(name, subset)
+    df = (pd.concat([
+        ds['train'].to_pandas()[['statement', 'subject']].assign(split="train"),
+        ds['validation'].to_pandas()[['statement', 'subject']].assign(split="valid")])
+         .rename(columns={'statement': 'text', 'subject': 'label'}))
+    df.to_csv(f"data/{name}.csv", index=False)
